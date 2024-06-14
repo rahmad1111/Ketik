aa660b1c4aeaa6e69f95a7f94c526bebc4c75be1
Arni Nazira <arninzra@gmail.com>
2024-06-14 17:28:53 +0700

    new

--- app/src/main/java/com/gkmobile/ketik/folder/newFolder.kt
+++ app/src/main/java/com/gkmobile/ketik/folder/newFolder.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -16,6 +17,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
@@ -49,6 +51,7 @@ fun NewFolderScreen() {
                 .padding(contentPadding)
                 .fillMaxSize()
                 .padding(15.dp)
+                .clip(RoundedCornerShape(10.dp))
         ) {
             FolderNameInput()
         }
